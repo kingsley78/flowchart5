4234475140ca724072adaa67e710b3784f03e715
kingsley78 <85633140+kingsley78@users.noreply.github.com>
2021-07-08 08:03:58 +0100

    Add files via upload

---
+++ flow5.py
@@ -0,0 +1,11 @@
+#flow chart5
+name = input("please enter name")
+if name == "alice":
+    print("hi alice")
+else:
+    age = int(input("please enter age"))
+    if age < 12:
+        print("you are alice,kiddo")
+    elif age > 200:
+        print("you are not alice grannie")
+    
